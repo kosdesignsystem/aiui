badde9066ac8ec2389676b7128751ec98641191e
Kasper <kasper@MacBook-Air-Max.local>
2026-03-11 14:53:23 +0300

    Фикс иконок

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  const repository = process.env.GITHUB_REPOSITORY?.split("/")[1];
-  const base =
-    process.env.VITE_BASE_PATH ??
-    (process.env.GITHUB_ACTIONS === "true" && repository ? `/${repository}/` : "/");
+export default defineConfig(({ command }) => {
+  const base = process.env.VITE_BASE_PATH ?? (command === "build" ? "./" : "/");
 
   return {
     base,
